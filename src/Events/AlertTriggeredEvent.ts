960843db8017b4448cd4e854fb52b35337edd95a
asabrina messikh <sab6401@gmail.com>
2024-12-05 11:16:01 +0100

    feat: ajout des events

---
+++ src/Events/AlertTriggeredEvent.ts
@@ -0,0 +1,69 @@
+import { DomainEvent } from "../Contexts/Shared/domain/DomainEvent";
+
+type AlertTriggeredDomainEventAttributes = {
+    readonly orderId: string;
+    readonly symbol: string;
+    readonly alertType: string;
+    readonly alertPrice: number;
+  };
+  export class AlertTriggeredDomainEvent extends DomainEvent {
+
+    static readonly EVENT_NAME = 'alert.triggered';
+  
+    readonly orderId: string;
+    readonly symbol: string;
+    readonly alertType: string;
+    readonly alertPrice: number;
+  
+    constructor({
+      aggregateId,
+      orderId,
+      symbol,
+      alertType,
+      alertPrice,
+      eventId,
+      occurredOn,
+    }: {
+      aggregateId: string;
+      orderId: string;
+      symbol: string;
+      alertType: string;
+      alertPrice: number;
+      eventId?: string;
+      occurredOn?: Date;
+    }) {
+      super({ eventName: AlertTriggeredDomainEvent.EVENT_NAME, aggregateId, eventId, occurredOn });
+      this.orderId = orderId;
+      this.symbol = symbol;
+      this.alertType = alertType;
+      this.alertPrice = alertPrice;
+    }
+  
+    toPrimitives(): AlertTriggeredDomainEventAttributes {
+      const { orderId, symbol, alertType, alertPrice } = this;
+      return {
+        orderId,
+        symbol,
+        alertType,
+        alertPrice,
+      };
+    }
+  
+    static fromPrimitives(params: {
+      aggregateId: string;
+      attributes: AlertTriggeredDomainEventAttributes;
+      eventId: string;
+      occurredOn: Date;
+    }): DomainEvent {
+      const { aggregateId, attributes, occurredOn, eventId } = params;
+      return new AlertTriggeredDomainEvent({
+        aggregateId,
+        orderId: attributes.orderId,
+        symbol: attributes.symbol,
+        alertType: attributes.alertType,
+        alertPrice: attributes.alertPrice,
+        eventId,
+        occurredOn,
+      });
+    }
+  }
---
+++ src/Events/TradeSignalEvent.ts
@@ -0,0 +1,90 @@
+import { DomainEvent } from "../Contexts/Shared/domain/DomainEvent";
+
+type TradeSignalDomainEventAttributes = {
+  readonly symbol: string;
+  readonly action: string;
+  readonly entryPrice: number;
+  readonly stopLoss: number;
+  readonly takeProfit: number;
+  readonly tradeAmount: number;
+  readonly strategyId: string;
+};
+
+export class TradeSignalDomainEvent extends DomainEvent {
+  static readonly EVENT_NAME = 'trade.signal';
+
+  readonly symbol: string;
+  readonly action: string;
+  readonly entryPrice: number;
+  readonly stopLoss: number;
+  readonly takeProfit: number;
+  readonly tradeAmount: number;
+  readonly strategyId: string;
+
+  constructor({
+    aggregateId,
+    symbol,
+    action,
+    entryPrice,
+    stopLoss,
+    takeProfit,
+    tradeAmount,
+    strategyId,
+    eventId,
+    occurredOn,
+  }: {
+    aggregateId: string;
+    symbol: string;
+    action: string;
+    entryPrice: number;
+    stopLoss: number;
+    takeProfit: number;
+    tradeAmount: number;
+    strategyId: string;
+    eventId?: string;
+    occurredOn?: Date;
+  }) {
+    super({ eventName: TradeSignalDomainEvent.EVENT_NAME, aggregateId, eventId, occurredOn });
+    this.symbol = symbol;
+    this.action = action;
+    this.entryPrice = entryPrice;
+    this.stopLoss = stopLoss;
+    this.takeProfit = takeProfit;
+    this.tradeAmount = tradeAmount;
+    this.strategyId = strategyId;
+  }
+
+  toPrimitives(): TradeSignalDomainEventAttributes {
+    const { symbol, action, entryPrice, stopLoss, takeProfit, tradeAmount, strategyId } = this;
+    return {
+      symbol,
+      action,
+      entryPrice,
+      stopLoss,
+      takeProfit,
+      tradeAmount,
+      strategyId,
+    };
+  }
+
+  static fromPrimitives(params: {
+    aggregateId: string;
+    attributes: TradeSignalDomainEventAttributes;
+    eventId: string;
+    occurredOn: Date;
+  }): DomainEvent {
+    const { aggregateId, attributes, occurredOn, eventId } = params;
+    return new TradeSignalDomainEvent({
+      aggregateId,
+      symbol: attributes.symbol,
+      action: attributes.action,
+      entryPrice: attributes.entryPrice,
+      stopLoss: attributes.stopLoss,
+      takeProfit: attributes.takeProfit,
+      tradeAmount: attributes.tradeAmount,
+      strategyId: attributes.strategyId,
+      eventId,
+      occurredOn,
+    });
+  }
+}

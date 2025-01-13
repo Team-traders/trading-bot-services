1612fb5f76a653a60ad7ae66b55e058c106a64eb
Souleymen Ouchane <souleymen@ouchane.fr>
2025-01-13 18:37:42 +0100

    Configure backend to listen for TradeSignalEvent

--- src/apps/tradeExecutionService/MoocBackendApp.ts
+++ src/apps/tradeExecutionService/MoocBackendApp.ts
@@ -3,6 +3,9 @@ import container from './dependency-injection';
 import { DomainEventSubscribers } from '../../Contexts/Shared/infrastructure/EventBus/DomainEventSubscribers';
 import { Server } from './server';
 import { RabbitMqConnection } from '../../Contexts/Shared/infrastructure/EventBus/RabbitMQ/RabbitMqConnection';
+import { ExecuteTradeOnTradeSignal } from '../../Contexts/tradeExecutionService/Courses/application/services/ExecuteTradeOnTradeSignal';
+import { BrokerAPI } from '../../Contexts/tradeExecutionService/Courses/application/services/BrokerAPI';
+import { MexcBroker } from '../../Contexts/tradeExecutionService/Courses/application/services/brokers/MexcBroker';
 
 export class MoocBackendApp {
   server?: Server;
@@ -11,7 +14,11 @@ export class MoocBackendApp {
     const port = process.env.PORT || '5004';
     this.server = new Server(port);
 
+    console.log('Starting server on port', port);
+
     await this.configureEventBus();
+
+    console.log('Event bus configured');
 
     return this.server.listen();
   }
@@ -21,6 +28,7 @@ export class MoocBackendApp {
   }
 
   async stop() {
+    console.log('Stopping server');
     const rabbitMQConnection = container.get<RabbitMqConnection>('TradeExecutionService.Shared.RabbitMQConnection');
     await rabbitMQConnection.close();
     return this.server?.stop();
@@ -31,6 +39,14 @@ export class MoocBackendApp {
     const rabbitMQConnection = container.get<RabbitMqConnection>('TradeExecutionService.Shared.RabbitMQConnection');
     await rabbitMQConnection.connect();
 
+    console.log('RabbitMQ connection established');
+
+    const brokerAPI: BrokerAPI = new MexcBroker();
+    const executeTradeOnTradeSignal = new ExecuteTradeOnTradeSignal(brokerAPI, eventBus);
+
     eventBus.addSubscribers(DomainEventSubscribers.from(container));
+    eventBus.addSubscribers(new DomainEventSubscribers([executeTradeOnTradeSignal]));
+
+    console.log('Subscribed to TradeSignalDomainEvent');
   }
 }
